0eac5126777930722b9223115e0c3472264ee86b
Horvath Mark <horvath.mark1@students.jedlik.eu>
2024-11-28 11:25:50 +0100

    beforeach

--- __tests/restaurant.controller.test.js
+++ __tests/restaurant.controller.test.js
@@ -4,6 +4,13 @@ const restaurantModel = require('../models/restaurantModel')
 const restaurantList = require('./mock-data/allRestaurants.json')
 
 restaurantModel.find = jest.fn()
+let req, res, next
+
+beforeEach(()=>{
+    req = httpMocks.createRequest()
+    res = httpMocks.createResponse()
+    next = null
+})
 
 
 describe('A getAll végponthoz tartozó metódus tesztelése', () => {
@@ -11,14 +18,12 @@ describe('A getAll végponthoz tartozó metódus tesztelése', () => {
         expect(typeof restaurantController.getAllRestaurant).toBe('function')
     })
     it('A getAllRestaurant függvényben meg kellene hívni a model find() függvényét', () =>{
-        let req, res, next
-        req = httpMocks.createRequest()
-        res = httpMocks.createResponse()
-        next = null
         restaurantController.getAllRestaurant(req, res, next)
         expect(restaurantModel.find).toHaveBeenCalled()
     })
-    it('A getAllRestaurant függvénynek vissza kellene adjon egy json listát az összes étteremmel és egy 200-as státuszkódot', ()=>{
+    it('A getAllRestaurant függvénynek vissza kellene adjon egy json listát az összes étteremmel és egy 200-as státuszkódot', async ()=>{
         restaurantModel.find.mockReturnValue(restaurantList)
+        await restaurantController.getAllRestaurant(req, res, next)
+        expect(res.statusCode).toBe(200)
     })
 })
